0d9028f7658d3d0bc94fdd6737a10a6b651cd4c0
Hunter Hoogendijk <hunter.hoogendijk@gmail.com>
2026-04-16 14:57:57 -0400

    Moved the audioContext and added analyzer

---
+++ tune-recall/app/audioProcessing.tsx
@@ -0,0 +1,14 @@
+export function setupAudioProcessing(stream: MediaStream): {
+  audioContext: AudioContext;
+  analyser: AnalyserNode;
+} {
+  const audioContext = new AudioContext();
+
+  const source = audioContext.createMediaStreamSource(stream);
+  const analyser = audioContext.createAnalyser();
+  analyser.fftSize = 2048;
+
+  source.connect(analyser);
+
+  return { audioContext, analyser };
+}
