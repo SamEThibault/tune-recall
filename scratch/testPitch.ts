5ea8f37941aaa7016c289abc9bc98a2975584128
Hunter Hoogendijk <hunter.hoogendijk@gmail.com>
2026-04-27 15:48:36 -0400

    restructured so its not nested

---
+++ scratch/testPitch.ts
@@ -0,0 +1,8 @@
+function detectPitch(buffer: Uint8Array, sampleRate: number): number {
+    
+    const normalized=new Float32Array(buffer.length)
+    // for (let i=0; i<buffer.length;i++) {
+    //     normalized[i]
+    // }
+
+}
